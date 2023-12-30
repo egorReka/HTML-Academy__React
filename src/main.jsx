913d80a71e00a62300cdfdd47054b2760badca36
egorReka <e.reka55@yandex.ru>
2023-12-30 23:39:14 +0300

    feat: add mock data

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ProductPage from './Product-page.jsx'
+import ProductPage from './Product-page'
 
+import product from './mock'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ProductPage />
+    <ProductPage product={product} />
   </React.StrictMode>,
 )
